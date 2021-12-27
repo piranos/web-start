118db612476e962110f879e73d322edef83cc0c2
David <d.eckhaus@farmedvisie.nl>
2021-12-27 16:35:52 +0100

    add backstop package

---
+++ src/MyClass.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace deckhaus\webStart;
+
+/**
+ * Description of MyClass
+ *
+ * @author deckhaus
+ */
+class MyClass {
+    
+    public function sayHello(string $name): sting
+    {
+        return sprintf("hallo %s", $name);
+    }
+}
